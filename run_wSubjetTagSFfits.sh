37bf2499a484c4f09ec831e7ac3ca194d4d79fc2
Ashley Marie Parker <ash.marie.parker@gmail.com>
2017-04-20 16:10:17 -0500

    commands to produce all pt bins and all WPs

---
+++ run_wSubjetTagSFfits.sh
@@ -0,0 +1,15 @@
+python wtagSFfits2.py --usePuppiSD -b --HP 0.55 --binmin 200 --binmax 300
+python wtagSFfits2.py --usePuppiSD -b --HP 0.4  --binmin 200 --binmax 300
+python wtagSFfits2.py --usePuppiSD -b --HP 0.35 --binmin 200 --binmax 300
+
+python wtagSFfits2.py --usePuppiSD -b --HP 0.55 --binmin 300 --binmax 500
+python wtagSFfits2.py --usePuppiSD -b --HP 0.4  --binmin 300 --binmax 500
+python wtagSFfits2.py --usePuppiSD -b --HP 0.35 --binmin 300 --binmax 500
+
+python wtagSFfits2.py --usePuppiSD -b --HP 0.55 --binmin 500 --binmax 1000
+python wtagSFfits2.py --usePuppiSD -b --HP 0.4  --binmin 500 --binmax 1000
+python wtagSFfits2.py --usePuppiSD -b --HP 0.35 --binmin 500 --binmax 1000
+
+python wtagSFfits2.py --usePuppiSD -b --HP 0.55 --binmin 200 --binmax 1000
+python wtagSFfits2.py --usePuppiSD -b --HP 0.4  --binmin 200 --binmax 1000
+python wtagSFfits2.py --usePuppiSD -b --HP 0.35 --binmin 200 --binmax 1000
